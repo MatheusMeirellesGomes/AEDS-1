f88ac994859a944a4b3e118bc6cd66c6bc2cfc77
Matheus Meirelles Gomes <matheusrmme2019@gmail.com>
2025-08-11 10:09:51 -0300

    att

---
+++ EXERCICIOS DO LIVRO/CAP 3 - CONCEITOS BASICOS EXERCICIOS RESOLVIDOS/exe.27-transformar horas.c
@@ -0,0 +1,9 @@
+//Receber uma hora formada por hora e minuto (numero real), calcular e mostrar a hora 
+//digitada apenas em minutos.
+//a) para quatro e meia, deve-se digitar 4.30; 
+//b) os minutos vão de 0 a 59; 
+
+int main()
+{
+    
+}
