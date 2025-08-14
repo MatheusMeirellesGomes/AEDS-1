bffcd75695032f4517d17346b68f8580bf1f2dda
Matheus Meirelles Gomes <matheusrmme2019@gmail.com>
2025-08-14 00:53:41 -0300

    att

--- EXERCICIOS DO LIVRO/CAPITULO 4 - EXERCICIOS RESOLVIDOS/exe4-Ordem Crescente.c
+++ EXERCICIOS DO LIVRO/CAPITULO 4 - EXERCICIOS RESOLVIDOS/exe4-Ordem Crescente.c
@@ -15,36 +15,36 @@ int main()
     printf("Terceiro Numero: "); 
     scanf("%d", &n3); 
     //Verificar para por em ordem crescente. 
-    if(n1 > n2 && n1 > n3)
+    if(n1 > n2 && n1 > n3)//Verificar primeiro número.
     {
         if(n2 > n3){
-            printf("A ordem crescente = %d, %d, %d\n", n1, n2, n3); 
+            printf("A ordem crescente = %d, %d, %d\n", n1, n2, n3); //Verificar se o segundo é maior que o terceiro. 
         }
         else{
-            printf("\nA ordem crescente = %d, %d, %d\n", n1, n3, n2); 
+            printf("\nA ordem crescente = %d, %d, %d\n", n1, n3, n2); //Se não for, inverter a ordem do segundo e terceito número.
         }
     }
-    else if(n2 > n1 && n2 > n3)
+    else if(n2 > n1 && n2 > n3)//Verificar segundo número. 
     {
         if(n1 > n3){
-             printf("\nA ordem crescente = %d, %d, %d\n", n2, n1, n3); 
+             printf("\nA ordem crescente = %d, %d, %d\n", n2, n1, n3); //Verificar se o primeiro é maior que o terceiro. 
         }
         else{
-            printf("\nA ordem crescente = %d, %D-d, %d\n", n2, n3, n1);
+            printf("\nA ordem crescente = %d, %D-d, %d\n", n2, n3, n1);//Se não for, inverter a ordem do primeiro e o terceiro número.
         }
     }
-    else if(n3 > n1 && n3 > n2)
+    else if(n3 > n1 && n3 > n2)//Verificar terceiro número.
     {
         if(n1 > n2){
-            printf("\nA ordem crescente = %d, %d, %d\n", n3, n1, n2);
+            printf("\nA ordem crescente = %d, %d, %d\n", n3, n1, n2);//Verificar se o primeiro é maior que o segundo.
         }
         else{
-            printf("\nA ordem crescente = %d, %d, %d\n", n3, n2, n1);
+            printf("\nA ordem crescente = %d, %d, %d\n", n3, n2, n1);//Se não for, inverter a ordem do primeiro e o segundo número.
         }
     }
     else 
     {
-        printf("\nOs numeros sao iguais.\n");
+        printf("\nOs numeros sao iguais.\n");//Se os números forem iguais, informar que eles são iguais. 
     }
 
     return 0; 
