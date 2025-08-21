3d47ae822442b591e271d608d470aa4ff59f5cad
Matheus Meirelles Gomes <matheusrmme2019@gmail.com>
2025-08-21 08:17:35 -0300

    + 5 exe e finalizado lista 00

---
+++ LISTA EXERCICIOS/LISTA 00/exercicio7.c
@@ -0,0 +1,23 @@
+//Ler a razão e o primeiro termo de uma PG e mostrar o seu quinto termo.
+//Fórmula da Pg: an​=a1​⋅q(n−1). Como é o 5º termo, seria a5 ​= a1​ * razao (elevado a quatro)
+
+int main()
+{
+    //Inicializar Variáveis.
+    int a1, a5, razao; 
+
+    //Receber informações necessárias. 
+    printf("Digite o primeiro termo: "); 
+    scanf("%d", &a1); 
+
+    printf("Digite a razao: "); 
+    scanf("%d", &razao); 
+
+    //Calcular para achar o quinto termo da PG. 
+    a5 = a1 * pow(razao,4); //quinto termo = primeiro termo multiplicado pela quarta potência da razão. 
+
+    //Imprimir resultado. 
+    printf("Quinto termo da PG = %d", a5); 
+
+    return 0;
+}
