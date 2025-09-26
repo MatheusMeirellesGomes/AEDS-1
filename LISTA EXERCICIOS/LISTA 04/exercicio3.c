05609ee6ea9da3a419bc6efc9c8a05d2f29eb772
Matheus Meirelles Gomes <matheusrmme2019@gmail.com>
2025-09-26 00:40:50 -0300

    att

---
+++ LISTA EXERCICIOS/LISTA 04/exercicio3.c
@@ -0,0 +1,31 @@
+int soma(int n)
+{
+    //Inicialzar Variáveis. 
+    int resposta;
+    //Indicar caso base. 
+    if (n == 1){
+        return 1; 
+    }
+    else {
+        for (int cont = 1; cont <= n; cont++)
+        {
+             resposta += cont; 
+        }
+        return resposta; 
+    }
+}
+int main()
+{
+    //Inicializar Variáveis.
+    int num; 
+
+    //Receber até qual número ele vai somar. 
+    printf("Informe ate onde vai somar: "); 
+    scanf("%d", &num); 
+
+    //Imprimir resultado acionando a recursão. 
+    printf("\nNumero: %d - Resultado da soma: %d\n", num, soma(num)); 
+
+    return 0;
+}
+
