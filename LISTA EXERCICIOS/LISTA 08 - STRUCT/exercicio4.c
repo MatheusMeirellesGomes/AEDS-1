160cf5392eb02067862ab74f87dd12b403926a4c
Matheus Meirelles Gomes <matheusrmme2019@gmail.com>
2025-11-14 14:55:03 -0300

    Checkpoint from VS Code for coding agent session

---
+++ LISTA EXERCICIOS/LISTA 08 - STRUCT/exercicio4.c
@@ -0,0 +1,13 @@
+//Uma determinada biblioteca possui obras de ciências exatas, humanas e biológicas, totalizando 1500 volumes, distribuídos em cada uma das áreas. 
+ //O proprietário resolveu agrupar as informações de cada livro no seguinte registro:
+ //• Código de catalogação
+ //• Doação (S/N)
+ //• Nome da obra
+ //• Nome do autor
+ //• Editora
+ //• Área
+ //Construir um algoritmo que:
+ //a) cadastre todos os volumes de cada uma das áreas em três vetores distintos
+ //b) permita ao usuário fazer consulta às informações cadastradas fornecendo o código de catalogação e a área. Existindo tal livro as informações são exibidas,
+ //caso contrário enviar mensagem de aviso. A consulta se repete até que o usuário digite código finalizador-1
+ 
